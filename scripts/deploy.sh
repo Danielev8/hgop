64032cd5d4f037b171e5ddebc77f2ae1fafc82c9
Daniel <danielevalberg@gmail.com>
2018-11-30 20:15:20 +0000

    put warning if it takes too long

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Destorying Terraform, please wait..."
+echo "Destorying Terraform, please wait... (If it is taking longer than expected, session token has likely timed out)"
 terraform destroy -auto-approve
 
-echo "Applying Terraform , please wait..."
+echo "Applying Terraform, please wait... (If it is taking longer than expected, session token has likely timed out)"
 terraform apply -auto-approve
 
 echo "Initializing a new instance on server"
