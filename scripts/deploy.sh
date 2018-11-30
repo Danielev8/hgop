38a2ff299eaeeaedb465c3d2d213ecb26d625a5a
Daniel <danielevalberg@gmail.com>
2018-11-30 20:53:27 +0000

    Added chmod for gamekeypair

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,7 @@ echo "Applying Terraform, please wait... (If it is taking longer than expected, 
 terraform apply -auto-approve
 
 echo "Initializing a new instance on server"
+chmod 400 ~/.aws/GameKeyPair.pem
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
+
+echo "API has been deployed"
