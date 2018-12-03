58f41e6bfa2fb5ad08bb9ebf18f9e519d3a71837
Einar Orri <einart16@ru.is>
2018-12-03 23:48:30 +0000

    added a unit test for lucky21

---
+++ game-api/lucky21.unit-test.js
@@ -0,0 +1,24 @@
+const deckConstructor = require('./deck.js');
+const dealerConstructor = require('./dealer.js');
+const lucky21Constructor = require('./lucky21.js');
+
+test('guess21OrUnder should draw the next card', () => {
+  // Arrange
+  let deck = deckConstructor();
+  deck = [
+      '05L', '01D', '09S', '10H', 
+  ];
+  let dealer = dealerConstructor();
+  // Override the shuffle to do nothing.
+  dealer.shuffle = (deck) => {};
+  
+  // Inject our dependencies
+  let game = lucky21Constructor(deck, dealer);
+  
+  // Act
+  game.guess21OrUnder(game);
+  
+  // Assert
+  expect(game.state.cards.length).toEqual(3);
+  expect(game.state.cards[2]).toEqual('01D');
+});
