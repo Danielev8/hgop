0fe74a629b0ab4d5f8ed600382de30542e2313a5
Daniel <danielevalberg@gmail.com>
2018-12-04 18:42:55 +0000

    fixed game initiation tests

--- game-api/lucky21.unit-test.js
+++ game-api/lucky21.unit-test.js
@@ -4,12 +4,16 @@ const lucky21Constructor = require('./lucky21.js');
 
 // test game initation
 test('a new game should have 50 cards left in the deck', () => {
-  let game = lucky21Constructer();
+  let deck = deckConstructor();
+  let dealer = dealerConstructor();
+  let game = lucky21Constructor(deck, dealer);
   expect(game.state.deck.length).toEqual(50);
 });
 
 test('a new game should have 2 drawn cards', () => {
-  let game = lucky21Constructer();
+  let deck = deckConstructor();
+  let dealer = dealerConstructor();
+  let game = lucky21Constructor(deck, dealer);
   expect(game.state.cards.length).toEqual(2);
 });
 
