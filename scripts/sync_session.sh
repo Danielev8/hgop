c6a737d8863bac0e7a4ba351251df09db9c02b76
Einar Orri <einart16@ru.is>
2018-12-05 16:47:36 +0000

    added a sync_session script

---
+++ scripts/sync_session.sh
@@ -0,0 +1,5 @@
+JENKINS_URL=# YOUR JENKINS URL
+
+scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
+ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo chmod a+r /var/lib/jenkins/.aws/credentials"
