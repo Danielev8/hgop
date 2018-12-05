00ef09e1feae359e1b762329768686e14c21f2ea
Einar Orri <einart16@ru.is>
2018-12-05 23:34:21 +0000

    added config to context.js

--- game-api/context.js
+++ game-api/context.js
@@ -7,6 +7,7 @@ const dealer = require('./dealer.js');
 const server = require('./server.js');
 const inject = require('./inject.js');
 const random = require('./random.js');
+const config = require('./config.js');
 
 module.exports = {
 	newContext: () => {
@@ -19,6 +20,7 @@ module.exports = {
 			'dealer': dealer,
 			'server': server,
 			'random': random,
+			'config': config,
 		});
 	},
 };
