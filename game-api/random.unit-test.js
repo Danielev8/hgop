2c7b61ff834196cbd04a301ca652d5f3035fff21
Einar Orri <einart16@ru.is>
2018-12-06 17:51:55 +0000

    added three unit test for the random.js

---
+++ game-api/random.unit-test.js
@@ -0,0 +1,29 @@
+let randomConstructor = require('./random.js');
+
+
+describe('Testing the random functionality', () => {
+	test('Random yields a number between 0 and 10', () => {
+		let min = 0;
+		let max = 10;
+		let newRandom = randomConstructor();
+		let randomInt = newRandom.randomInt;
+		expect(randomInt(min, max)).toBeLessThanOrEqual(max);
+		expect(randomInt(min, max)).toBeGreaterThanOrEqual(min);
+	});
+	test('Random yiels a number that is exactly 10', () => {
+		let min = 10;
+		let max = 10;
+		let newRandom = randomConstructor();
+		let randomInt = newRandom.randomInt;
+		expect(randomInt(min, max)).toEqual(max);
+		expect(randomInt(min, max)).toEqual(min);
+	});
+	test('Random yiels a number between 0 and 1', () => {
+		let min = 0;
+		let max = 1;
+		let newRandom = randomConstructor();
+		let randomInt = newRandom.randomInt;
+		expect(randomInt(min, max)).toBeLessThanOrEqual(max);
+		expect(randomInt(min, max)).toBeGreaterThanOrEqual(min);
+	});
+});
