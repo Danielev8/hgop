f99e9d5a6fb76c6e97d1dd831f488904644d6fac
Daniel <danielevalberg@gmail.com>
2018-12-07 23:41:17 +0000

    added exit if docker not present

--- scripts/initialize_game_api_instance.sh
+++ scripts/initialize_game_api_instance.sh
@@ -21,4 +21,10 @@ sudo usermod -aG docker ubuntu
 
 echo 'Install Docker Compose'
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose || exit 1
+sudo chmod +x /usr/local/bin/docker-compose
+
+if [[ ! -f docker-compose.yml ]] ; 
+then
+    echo "docker-compose.yml is not present, exiting..."
+    exit 1
+fi
