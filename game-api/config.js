acf98f4f41c6b8987603cd395d8a5e41c2999dc8
Daniel <danielv@Daniels-MacBook-Pro.local>
2018-12-10 17:04:08 +0000

    fixed debug message

--- game-api/config.js
+++ game-api/config.js
@@ -1,5 +1,5 @@
 module.exports = function (context) {
-	console.log(String(process.env));
+	console.log(JSON.stringify(process.env));
 	return {
 		// Postgres
 		pgHost: process.env.POSTGRES_HOST || 'default_host',
