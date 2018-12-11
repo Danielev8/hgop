f8495f25620fa8c11a3c55da02b59fbd868ef430
Daniel <danielevalberg@gmail.com>
2018-12-11 20:46:25 +0000

    fixed connections not closing and fixed queries

--- game-api/database.js
+++ game-api/database.js
@@ -11,26 +11,17 @@ module.exports = function (context) {
 			database: config.pgDatabase,
 		});
 	}
-	/*
+
 	let client = getClient();
-		setTimeout(() =>
-			client.connect((err) => {
-				if (err) {
-					console.log('failed to connect to postgres!');
-				} else {
-					console.log('successfully connected to postgres!');
-					client.query('CREATE TABLE IF NOT EXISTS GameResults (ID SERIAL PRIMARY KEY, Won BOOL NOT NULL, Score INT NOT NULL, Total INT NOT NULL, InsertedDate TIMESTAMP NOT NULL);', (err) => {
-						if (err) {
-							console.log('error creating game result table!');
-						} else {
-							console.log('successfully created game result table!');
-						}
-						client.end();
-					});
-				}
-			}), 5000);
-	*/
-	// Function returns total count and takes in "WHERE X = Y" as parameters
+	setTimeout(() =>
+		client.connect((err) => {
+			if (err) {
+				console.log('failed to connect to postgres!');
+			} else {
+				console.log('successfully connected to postgres!');
+			}
+		}), 5000);
+	// Function returns total count and takes in queries as parameters
 	const sendQuery = (onSuccess, onError, query) => {
 		let client = getClient();
 		client.connect((err) => {
@@ -42,7 +33,7 @@ module.exports = function (context) {
 					if (err) {
 						onError();
 					} else {
-						return res.rows[0] ? onSuccess(res.rows[0].count) : onSuccess();
+						res.rows[0] ? onSuccess(res.rows[0].count) : onSuccess();
 					}
 					client.end();
 				});
@@ -71,14 +62,14 @@ module.exports = function (context) {
 		// Should call onSuccess with integer.
 		getTotalNumberOfWins: (onSuccess, onError) => {
 			const query = {
-				text: 'SELECT COUNT(*) FROM "GameResult" r WHERE r.Won = TRUE;'
+				text: 'SELECT COUNT(*) FROM "GameResult" WHERE "Won" = TRUE;'
 			};
 			return sendQuery(onSuccess, onError, query);
 		},
 		// Should call onSuccess with integer.
 		getTotalNumberOf21: (onSuccess, onError) => {
 			const query = {
-				text: 'SELECT COUNT(*) FROM "GameResult" r WHERE r.Total = 21'
+				text: 'SELECT COUNT(*) FROM "GameResult" WHERE "Total" = 21;'
 			};
 			return sendQuery(onSuccess, onError, query);
 		},
