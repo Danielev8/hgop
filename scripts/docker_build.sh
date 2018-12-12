4a820392e936026cd659b8b25c4fbfdb5550d897
Daniel <danielevalberg@gmail.com>
2018-12-12 09:14:25 +0000

    added client build and push

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,3 +3,6 @@
 GIT_COMMIT=$1
 cd game-api || exit 1
 docker build -t danielev22/hgop:$GIT_COMMIT . || exit 1
+
+cd ../game-client || exit 1
+docker build -t danielev22/hgop:UI$GIT_COMMIT . || exit 1
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,3 +2,4 @@
 
 GIT_COMMIT=$1
 docker push danielev22/hgop:$GIT_COMMIT || exit 1
+docker push danielev22/hgop:UI$GIT_COMMIT || exit 1
