4b17231005cb3615c7c52eb4af855452a96903a4
Daniel <danielevalberg@gmail.com>
2018-12-12 05:19:15 +0000

    removed later header

--- game-api/server.js
+++ game-api/server.js
@@ -10,7 +10,6 @@ module.exports = function (context) {
 
 	app.use((req, res, next) => {
 		res.header('Access-Control-Allow-Origin', "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		next();
 	});
 
